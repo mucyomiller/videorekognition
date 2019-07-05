1cfd4b7e6b8f918a2fcfb0c1eb1465aa1efd4640
Mucyo Miller <mucyomiller@gmail.com>
2019-07-06 01:32:01 +0200

    adds a bunch of utility end points

--- src/index.js
+++ src/index.js
@@ -15,8 +15,78 @@ app.use(express.urlencoded({
 const PORT = process.env.PORT || 3000;
 
 const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-east-1' });
+export const rekognition = new AWS.Rekognition();
 
-var rekognition = new AWS.Rekognition();
+// IndexFaces End Points
+app.post('/indexface',(req, res) => {
+
+ var params = {
+    CollectionId: "persons_of_interests", 
+    DetectionAttributes: [
+    ], 
+    Image: {
+     S3Object: {
+      Bucket: req.body.bucket_name || 'somuga-persons-of-interests', 
+      Name: req.body.image_name || 'mucyomiller.jpeg'
+     }
+    }
+   };
+   rekognition.indexFaces(params,(err, data) => {
+     if (!err){
+         res.json(data);
+     }
+     res.json(err);
+   });
+});
+
+app.get('/listFaces', (req, res) => {
+    var params = {
+        CollectionId: 'persons_of_interests'
+      };
+      rekognition.listFaces(params, (err, data) => {
+        if(!err){
+            res.json(data);
+        }
+        res.json({status: 404, message: 'Resource not found'});
+      });
+});
+
+app.get('/listStreamProcessors', (req, res) => {
+    var params = {
+        MaxResults: 10
+      };
+      rekognition.listStreamProcessors(params,(err, data) => {
+        if (!err){
+            res.json(data)
+        }
+        res.json(err);
+      });
+});
+
+app.post('/startStreamProcessor', (req, res) =>{
+    var params = {
+        Name: req.body.stream_processor_name || 'mistreamprocessor'
+      };
+      rekognition.startStreamProcessor(params, (err, data) => {
+        if (!err){
+            res.json(data);
+        }
+        res.json(err);
+      });
+});
+
+app.post('/stopStreamProcessor', (req, res) => {
+    var params = {
+        Name: req.body.stream_processor_name || 'mistreamprocessor'
+      };
+      rekognition.stopStreamProcessor(params, (err, data) =>  {
+        if (!err){
+            res.json(data);
+        }
+        res.json(err);
+      });
+});
 
 
 
